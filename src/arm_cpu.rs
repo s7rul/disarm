c6d4e5a59b5dd2867ae6684f400f8a93b521cc57
s7rul <eriser-8@student.ltu.se>
2023-01-13 00:10:54 +0100

    ready for demo

--- src/arm_cpu.rs
+++ src/arm_cpu.rs
@@ -623,12 +623,14 @@ fn align(x: u32, y: u32) -> u32 {
 #[test]
 fn demo_test() {
     let mut chunk: &[u8] = &[
-        0x0f, 0x20, // mov r0, #10
+        //0x09, 0x20, // mov r0, #9
+        0x0a, 0x20, // mov r0, #10
+        //0x0e, 0x20, // mov r0, #14
         0x0a, 0x28, // cmp	r0, #10
         0x02, 0xd8, // bhi	0x10e <cond_function+0xa> @ imm = #4
         0x01, 0x21, // movs	r1, #1
         0x08, 0x18, // adds	r0, r1, r0
-        0x70, 0x47, // bx	lr
+        0x00, 0xbe, // bkpt 0
         0x00, 0x21, // movs	r1, #0
         0xc9, 0x43, // mvns	r1, r1
         0x08, 0x18, // adds	r0, r1, r0
